d8f05c60c2da5812ea30be35e536fce9ca077da3
tranlam <tranlam@Debian>
2022-12-14 16:37:21 +0700

    token long term

--- index.js
+++ index.js
@@ -16,31 +16,41 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 campaigns = data.data
-delay = 1000*60*60
+
+
 app.get('/', (req, res) => {
-    res.json({'success': true})
+    res.json({ 'success': true })
 })
 
 main = async () => {
     i = 0
     count = 0
     fail = 0
+    delay = 1000 * 60 * 60
     function myLoop() {
         setTimeout(async () => {
+            //check array đến cuối mảng
             if (campaigns[i]) {
-                await handlePostFacebook(accessToken, campaigns[i].name, campaigns[i].link)
-                    .then(data => {
-                        console.log(data)
-                    })
-                    .catch(err => {
-                        fail++
-                        console.log(err)
-                })
+                //check url có bị block không?
+                if (campaigns[i]?.block != true) {
+                    await handlePostFacebook(accessToken, campaigns[i].name, campaigns[i].link)
+                        .then(data => {
+                            console.log(`www.facebook.com/${data.id}`)
+                            count++;
+                        })
+                        .catch(err => {
+                            fail++
+                            console.log(err)
+                        })
+                } else {
+                    console.log(`name ${campaigns[i].name} bị block url`)
+                }
                 i++;
-                count++;
-                console.log(`name ${campaigns[i].name}, tông ${count}, fail ${fail}`)
-            } else i=0
-            if( fail <10 ) {
+                console.log(`name ${campaigns[i-1].name}, tông ${count}, fail ${fail}`)
+            } else i = 0
+
+            // fail quá nhiều sẽ dừng lại
+            if (fail < 10) {
                 myLoop();
             }
         }, delay)
