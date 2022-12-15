ccadee5d7dd665142d311cb6781d98ed010e1e08
tranlam <tranlam@Debian>
2022-12-15 15:19:47 +0700

    test post mongodb

--- modules/loadFinancialServicesCampaigns.js
+++ modules/loadFinancialServicesCampaigns.js
@@ -1,7 +1,7 @@
 const FinancialServicesCampaignsSchema = require("../models/Campaigns")
 
 const loadFinancialServicesCampaigns = async () => {
-    const campaigns = await FinancialServicesCampaignsSchema.find({idCampaign: "5141521349430660949"})
+    const campaigns = await FinancialServicesCampaignsSchema.find()
     return campaigns
 }
 
