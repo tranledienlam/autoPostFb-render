001eaeeef9309581f917c8c286df2bb2e73fd3bb
tranlam <tranlam@Debian>
2022-12-16 10:23:37 +0700

    Initial commit

--- modules/formatTime.js
+++ modules/formatTime.js
@@ -3,7 +3,7 @@ const formatTime = (time) => {
     sencond = 0
     minute =0
     hour = 0
-    
+
     if(time % 60 >= 0) {
         sencond = time % 60
         minute = (time - (time % 60))/60
@@ -12,7 +12,9 @@ const formatTime = (time) => {
         hour = (minute - (minute % 60))/60
         minute = minute % 60
     }
+
     return `${hour}h:${minute}m:${sencond}`
+
 }
 
 module.exports = formatTime
