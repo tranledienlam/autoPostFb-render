7ac6753f5d0794f62b33d32ef093ebd8638c6ef6
TranLam <lam.tranledien@gmail.com>
2023-03-04 12:06:11 +0700

    nguyenvanlong

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
 exports.PORT= process.env.PORT || '4000'
-exports.MONGODB = 'mongodb+srv://anhthong78910:anhthong78910@cluster0.8cgnr7b.mongodb.net/campaigns'
+exports.MONGODB = 'mongodb+srv://admin:admin@cluster0.zibho6i.mongodb.net/campaigns'
