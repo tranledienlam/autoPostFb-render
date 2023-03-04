1d673be9f4818fb9a5ecf819bae3a3df1f585783
tranledienlam <116325074+tranledienlam@users.noreply.github.com>
2023-03-04 12:48:01 +0700

    hanh ngan

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
 exports.PORT= process.env.PORT || '4000'
-exports.MONGODB = 'mongodb+srv://admin:admin@cluster0.dz77ruh.mongodb.net/campaigns'
+exports.MONGODB = 'mongodb+srv://admin:admin@cluster0.uzkhfnq.mongodb.net/campaigns'
