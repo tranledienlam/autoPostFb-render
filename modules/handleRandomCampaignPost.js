d1e26e21615e5c68944739489caad2f92d561fc6
TranLam <lam.tranledien@gmail.com>
2023-03-05 07:24:26 +0700

    lengocson

--- modules/handleRandomCampaignPost.js
+++
@@ -1,8 +0,0 @@
-const handleRandomCampaignPost = async (campagins) => {
-
-    index = Math.floor(Math.random()* campagins.length)
-
-    return index
-}
-
-module.exports = handleRandomCampaignPost
